cabd581576b37bc1f20b091664fc5b4f3aa6fa8d
Amir-Aram <wannabeccie5@gmail.com>
2022-01-08 11:50:57 +0100

    asd

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pdfkit as pd 
 
 url = input("copy your wiki URL and paste it here:  \n")
-file_name = input("what do you want to call your file? \n")
+
+file_name = input("what do you want to name your file? \n")
+
 file_name = file_name + ".pdf"
 
 pd.from_url(url, file_name)
